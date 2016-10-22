d7c752f4e04ae58a5ac029b9f60e31cf78663087
poslavsky <mishapsv@gmail.com>
2016-10-22 15:08:50 +0300

    исправил ошибки еслинта

--- src/js/resizer.js
+++ src/js/resizer.js
@@ -130,12 +130,11 @@
       //задаем цвет и прозрачность фона
       this._ctx.fillStyle = 'rgba(0,0,0,.8)';
       this._ctx.beginPath();
-      var sideInner =
-      this._ctx.rect((-this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2 - this._ctx.lineWidth/2,
-          (-this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2 - this._ctx.lineWidth/2,
+      this._ctx.rect((-this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2 - this._ctx.lineWidth / 2,
+          (-this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2 - this._ctx.lineWidth / 2,
           this._resizeConstraint.side - this._ctx.lineWidth / 2 + this._ctx.lineWidth,
           this._resizeConstraint.side - this._ctx.lineWidth / 2 + this._ctx.lineWidth);
-      this._ctx.rect((-this._container.width / 2+this._container.width),
+      this._ctx.rect((-this._container.width / 2 + this._container.width),
           (-this._container.height / 2),
           -this._container.width,
           this._container.height);
@@ -143,10 +142,10 @@
 
       // Текст с размерами смещаем на deltaX по оси Х (measureText().width)
       // Аналогично подобрали смещение по У (расположение подобрано)
-      var sizeXY = this._container.width + " x " + this._container.height;
+      var sizeXY = this._container.width + ' x ' + this._container.height;
       var deltaX = -this._ctx.measureText(sizeXY).width;
-      var deltaY = -(this._resizeConstraint.side/2+10);
-      this._ctx.fillStyle = "#fff";
+      var deltaY = -(this._resizeConstraint.side / 2 + 10);
+      this._ctx.fillStyle = '#fff';
       this._ctx.font = 'normal 18px sans-serif';
       this._ctx.fillText(sizeXY, deltaX, deltaY);
 
