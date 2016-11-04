9b1e3c29a072ab704dcdc8037d9067e29984d16c
poslavsky <mishapsv@gmail.com>
2016-11-04 20:27:11 +0300

    разбил джиэс на модули

---
+++ src/js/picture.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var getPicturesElement = function(pic) {
+  var template = document.querySelector('template');
+  var templateContainer = 'content' in template ? template.content : template;
+  var picElement = templateContainer.querySelector('.picture').cloneNode(true);
+
+  picElement.querySelector('.picture-comments').textContent = pic.comments;
+  picElement.querySelector('.picture-likes').textContent = pic.likes;
+
+  var backgroundImage = new Image();
+  backgroundImage.onload = function() {
+    backgroundImage.height = 182;
+    backgroundImage.width = 182;
+  };
+  backgroundImage.onerror = function() {
+    picElement.classList.add('picture-load-failure');
+  };
+  backgroundImage.src = pic.url;
+  picElement.replaceChild(backgroundImage, picElement.querySelector('img'));
+  return picElement;
+};
+
+module.exports = getPicturesElement;
--- src/js/pictures.js
+++ src/js/pictures.js
@@ -1,51 +1,21 @@
 'use strict';
-(function() {
-  var PICS_LOAD_URL = 'http://localhost:1507/api/pictures';
-  var filters = document.querySelector('.filters');
-  var container = document.querySelector('.pictures');
-  var template = document.querySelector('template');
-  filters.classList.add('hidden');
-  var templateContainer = 'content' in template ? template.content : template;
-  var getPicturesElement = function(pic) {
-    var picElement = templateContainer.querySelector('.picture').cloneNode(true);
-    picElement.querySelector('.picture-comments').textContent = pic.comments;
-    picElement.querySelector('.picture-likes').textContent = pic.likes;
-    var backgroundImage = new Image();
-    backgroundImage.onload = function() {
-      backgroundImage.height = 182;
-      backgroundImage.width = 182;
-    };
-    backgroundImage.onerror = function() {
-      picElement.classList.add('picture-load-failure');
-    };
-    backgroundImage.src = pic.url;
-    picElement.replaceChild(backgroundImage, picElement.querySelector('img'));
-    return picElement;
-  };
-  var renderPictures = function(arrayElements) {
-    arrayElements.forEach(function(pic) {
-      container.appendChild(getPicturesElement(pic));
-    });
-  };
-  // renderPictures(pictures);
 
-  var load = function(url, callback, callbackName) {
-    if (!callbackName) {
-      callbackName = 'cb' + Date.now();
-    }
+var getPicturesElement = require('./picture');
+var load = require('./load');
 
-    window[callbackName] = function(data) {
-      callback(data);
-    };
+var PICS_LOAD_URL = 'http://localhost:1507/api/pictures';
+var container = document.querySelector('.pictures');
+var filters = document.querySelector('.filters');
 
-    var script = document.createElement('script');
-    script.src = url + '?callback=' + callbackName;
-    document.body.appendChild(script);
-  };
+filters.classList.add('hidden');
 
-  load(PICS_LOAD_URL, function(data) {
-    renderPictures(data);
-  }, '__jsonpCallback');
-
+var renderPictures = function(arrayElements) {
+  arrayElements.forEach(function(pic) {
+    container.appendChild(getPicturesElement(pic));
+  });
   filters.classList.remove('hidden');
-})();
+};
+
+load(PICS_LOAD_URL, renderPictures, '__jsonpCallback');
+
+module.exports = renderPictures;
