647a34c845016f9557486255f693a20a9138fd71
poslavsky <mishapsv@gmail.com>
2016-11-11 17:44:45 +0300

    getPicturesElement положил в прототип

--- src/js/picture.js
+++ src/js/picture.js
@@ -4,7 +4,7 @@ var gallery = require('./gallery');
 
 var Picture = function(data, number) {
   this.data = data;
-  this.element = getPicturesElement(data, number);
+  this.element = this.getPicturesElement(data, number);
   var self = this;
   this.element.onclick = function(e) {
     e.preventDefault();
@@ -15,29 +15,30 @@ var Picture = function(data, number) {
   };
 };
 
-var getPicturesElement = function(pic, number) {
-  var template = document.querySelector('template');
-  var templateContainer = 'content' in template ? template.content : template;
-  var picElement = templateContainer.querySelector('.picture').cloneNode(true);
-  picElement.onclick = function(e) {
-    e.preventDefault();
-    gallery.show(number);
-  };
-  picElement.querySelector('.picture-comments').textContent = pic.comments;
-  picElement.querySelector('.picture-likes').textContent = pic.likes;
+Picture.prototype = {
+  getPicturesElement: function(pic, number) {
+    var template = document.querySelector('template');
+    var templateContainer = 'content' in template ? template.content : template;
+    var picElement = templateContainer.querySelector('.picture').cloneNode(true);
+    picElement.onclick = function(e) {
+      e.preventDefault();
+      gallery.show(number);
+    };
+    picElement.querySelector('.picture-comments').textContent = pic.comments;
+    picElement.querySelector('.picture-likes').textContent = pic.likes;
 
-  var backgroundImage = new Image();
-  backgroundImage.onload = function() {
-    backgroundImage.height = 182;
-    backgroundImage.width = 182;
-  };
-  backgroundImage.onerror = function() {
-    picElement.classList.add('picture-load-failure');
-  };
-  backgroundImage.src = pic.url;
-  picElement.replaceChild(backgroundImage, picElement.querySelector('img'));
-  return picElement;
+    var backgroundImage = new Image();
+    backgroundImage.onload = function() {
+      backgroundImage.height = 182;
+      backgroundImage.width = 182;
+    };
+    backgroundImage.onerror = function() {
+      picElement.classList.add('picture-load-failure');
+    };
+    backgroundImage.src = pic.url;
+    picElement.replaceChild(backgroundImage, picElement.querySelector('img'));
+    return picElement;
+  }
 };
 
-//module.exports = getPicturesElement;
 module.exports = Picture;
